3c09ca200a3196d7fe309d16658eb9cd1629cbed
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-12 13:59:47 +0300

    Dark mode

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Script from './Script'
 import './style.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+
+const styles ={
+
+  global: (props) =>({
+
+    body:{
+
+      bg:mode("gray.100", "#000")(props),
+      color:mode("gray.800", "#fff")(props)
+
+    }
+
+  })
+
+}
+
+
+const config ={
+
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+
+}
+
+const theme = extendTheme({ config, styles })
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
 
       <Script/>
 
