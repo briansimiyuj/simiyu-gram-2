0f5a7664889a33ff3fdead573f551d4143d27f07
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-13 16:06:15 +0300

    sidebar 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,42 @@
+import { Box, Flex, Link } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
+
 const Sidebar = () =>{
 
     return(
 
-        <div className="side-bar">side-bar</div>
+        <Box
+            height={"100vh"}
+            borderRight={"1px solid"}
+            borderColor={"whiteAlpha.300"}
+            py={8}
+            position={"sticky"}
+            top={0}
+            left={0}
+            px={{base: 2, md: 4}}
+        >
+
+            <Flex 
+                direction={"column"} 
+                gap={10}
+                width={"full"}
+                height={"full"}
+            >
+
+                <Link 
+                    to="/"
+                    as={RouterLink}
+                    display={{base: "none", md: "block"}}
+                    cursor={"pointer"}
+                >
+
+                    <h1 style={{fontSize:"25px", fontWeight: 700, fontStyle: "italic"}}>SimiyuGram</h1>
+
+                </Link>
+
+            </Flex>
+
+        </Box>
 
     )
 
