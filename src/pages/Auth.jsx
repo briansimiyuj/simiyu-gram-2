5ec1f2a37495630058421e854dcd3cfe4bf5eeab
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-13 08:08:04 +0300

    AppStore images

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,8 @@
-import { Flex, Container, Box, Image } from '@chakra-ui/react'
-import auth from '../../img/auth.png';
+import { Flex, Container, Box, Image, VStack } from '@chakra-ui/react'
+import auth from '../../img/auth.png'
+import playstore from '../../img/playstore.png'
+import microsoft from '../../img/microsoft.png'
+import AuthForm from '../components/AuthForm'
 
 const Auth = () =>{
 
@@ -9,11 +12,34 @@ const Auth = () =>{
 
             <Container maxW={"container.md"} padding={0}>
 
-                <Box display={{base:"none", md:"block"}}>
+                <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
 
-                    <Image src={auth} htmlHeight={650} alt="phone"/>
+                    <Box display={{base:"none", md:"block"}}>
 
-                </Box>
+                        <Image src={auth} htmlHeight={650} alt="phone"/>
+
+                    </Box>
+                
+                
+                
+                    <VStack spacing={4} align={"stretch"}>
+
+                        <AuthForm/>
+
+                        <Box textAlign={"center"}>Get The App</Box>
+
+
+                        <Flex gap={5} justifyContent={"center"}>
+
+                            <Image src={playstore} alt="playstore logo" h={10}/>
+                            
+                            <Image src={microsoft} alt="microsoft logo" h={10}/>
+
+                        </Flex>
+
+                    </VStack>
+
+                </Flex>
 
             </Container>
 
