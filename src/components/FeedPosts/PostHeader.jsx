1183098ede7ed415ac75a62f29783c853de6525f
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-15 12:38:06 +0300

    Post Header Content Structure

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,8 +1,42 @@
+import { Flex, Box, Avatar, Text } from '@chakra-ui/react'
+import img1 from '../../../img/img1.png'
+
 const PostHeader = () =>{
 
     return(
 
-        <h1>PostHeader</h1>
+        <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
+
+            <Flex alignItems={"center"} gap={2}>
+
+                <Avatar src={img1} alt='user profile pic' size={"sm"}/>
+
+                <Flex fontSize={12} fontWeight={"bold"} gap={2}>
+
+                    <h4>JenniferPauline</h4>
+
+                    <Box color={"gray.500"}>• 10 minutes ago</Box>
+
+                </Flex>
+
+            </Flex>
+
+            
+            <Box
+                cursor={"pointer"}
+            >
+
+                <Text 
+                    fontSize={12} 
+                    fontWeight={"bold"} 
+                    color={"blue.500"}
+                    _hover={{color: "white"}}
+                    transition={"0.2s ease-in-out"}
+                >Follow</Text>
+
+            </Box>
+
+        </Flex>
 
     )
 
