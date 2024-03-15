5acbeb22b1617a67d8845fe8aabcf57546e4c1b2
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-15 10:10:22 +0300

    Homepage structure

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 import Auth from './pages/Auth'
 import PageLayout from './PageLayout/PageLayout'
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import { Container, Flex, Box } from '@chakra-ui/react'
+
+const Home = () =>{
+
+    return(
+
+        <Container maxW={"container.lg"}>
+
+            <Flex gap={20}>
+
+                <Box flex={2} py={10} border={"1px solid blue"}>FeedPosts</Box>
+
+                <Box 
+                    flex={3} 
+                    mr={20}
+                    display={{ base: "none", lg: "block" }}
+                    maxW={"300px"}
+                    border={"1px solid red"}
+                >SuggestedUsers</Box>
+
+            </Flex>
+
+        </Container>
+
+    )
+
+}
+
+export default Home
