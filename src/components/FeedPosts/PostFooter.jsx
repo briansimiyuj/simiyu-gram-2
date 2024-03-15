3677e9f6b27496d1bf4953ef3c9522292ea2645f
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-15 13:55:23 +0300

    Post Comments UI 2

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text } from '@chakra-ui/react'
+import { Flex, Box, Text, InputGroup, InputRightElement, Input, Button } from '@chakra-ui/react'
 import { NotificationsLogo, UnlikeLogo, CommentLogo } from '../../assets/constants'
 import { useState } from'react'
 
@@ -71,6 +71,35 @@ const PostFooter = () =>{
                 <Text fontSize='sm' color={"gray"}>View all 1,000 comments</Text>
 
             </Text>
+
+
+            <Flex
+                alignItems={"center"}
+                gap={2} 
+                justifyContent={"space-between"}
+                w={"full"}
+            >
+
+                <InputGroup>
+                
+                    <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14}/>
+
+                    <InputRightElement>
+                    
+                        <Button
+                            fontSize={14}
+                            color={"blue.500"}
+                            fontWeight={600}
+                            cursor={"pointer"}
+                            _hover={{color: "white"}}
+                            bg={"transparent"}
+                        >Post</Button>
+                    
+                    </InputRightElement>
+                
+                </InputGroup>
+                
+            </Flex>
         
         </>
 
