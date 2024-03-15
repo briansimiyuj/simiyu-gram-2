1cbdedddb060483c78803944476f221294661bd8
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-15 13:10:58 +0300

    Like post UI

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,8 +1,38 @@
+import { Flex, Box } from '@chakra-ui/react'
+import { NotificationsLogo, UnlikeLogo } from '../../assets/constants'
+import { useState } from'react'
+
 const PostFooter = () =>{
+
+    const [liked, setLiked] = useState(false),
+          [likes, setLikes] = useState(1000)
 
     return(
 
-        <h1>PostFooter</h1>
+        <>
+        
+            <Flex
+                alignItems={"center"}
+                gap={4}
+                w={"full"}
+                pt={0}
+                mb={2}
+                mt={"auto"}
+            >
+
+                <Box>
+
+                    {
+
+                        !liked? (<NotificationsLogo/>) : (<UnlikeLogo/>)
+
+                    }
+
+                </Box>
+
+            </Flex>
+        
+        </>
 
     )
 
