03e9b076c56269272fff9d0ac94d610b735c043c
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-16 16:12:05 +0300

    Set loading 1

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,4 +1,5 @@
-import { Container } from '@chakra-ui/react'
+import { Container, VStack, Flex, SkeletonCircle, Skeleton, Box } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
 import FeedPost from './FeedPost'
 import img2 from '../../../img/img2.png'
 import img4 from '../../../img/img4.png'
@@ -7,6 +8,20 @@ import img1 from '../../../img/img1.png'
 
 const FeedPosts = () =>{
 
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() =>{
+    
+        setTimeout(() =>{
+
+            setIsLoading(false)
+
+        }, 2000)
+    
+    }, [])
+
+
     return(
 
         <Container 
@@ -14,6 +29,40 @@ const FeedPosts = () =>{
             py={10} 
             px={2}
         >
+
+            {
+
+                isLoading && [0, 1, 2, 3, 4].map(i =>(
+
+                    <VStack key={i} gap={4} alignItems={"start"} mb={10}>
+
+                        <Flex gap={2}>
+
+                            <SkeletonCircle size="10"/>
+
+                            
+                            <VStack gap={2} alignItems={"start"}>
+
+                                <Skeleton w={"200px"} h={"10px"}/>
+
+                                <Skeleton w={"200px"} h={"10px"}/>
+
+                            </VStack>
+
+                        </Flex>
+
+
+                        <Skeleton w={"full"}>
+
+                            <Box h={"500px"}>Contents wrapped</Box>
+
+                        </Skeleton>
+
+                    </VStack>
+
+                ))
+
+            }
 
             <FeedPost img={img1} username="ChrisKen" avatar={img1}/>
             
