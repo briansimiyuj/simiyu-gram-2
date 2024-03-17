5d3cf1b90d2d094b1cf9bca3778eb84504b7e346
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-17 18:00:58 +0300

    Suggested for you

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,4 +1,4 @@
-import { VStack } from '@chakra-ui/react'
+import { VStack, Flex, Text } from '@chakra-ui/react'
 
 import SuggestedHeader from './SuggestedHeader'
 
@@ -9,6 +9,14 @@ const SuggestedUsers = () =>{
         <VStack py={8} px={6} gap={4}>
 
             <SuggestedHeader/>
+
+            <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+
+                <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>Suggested For You</Text>
+
+                <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>See All</Text>
+
+            </Flex>
 
         </VStack>
 
