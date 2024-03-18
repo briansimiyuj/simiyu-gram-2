74ecf3567eb0ebf04f73a71601be688f4d49fe56
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-18 11:38:04 +0300

    Profile Header Structure 2

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,4 +1,4 @@
-import { Flex, AvatarGroup, Avatar } from '@chakra-ui/react'
+import { Flex, AvatarGroup, Avatar, VStack, Text, Button } from '@chakra-ui/react'
 import profilepic from '../../../img/profilepic.png'
 
 const ProfileHeader = () =>{
@@ -21,6 +21,40 @@ const ProfileHeader = () =>{
                 <Avatar src={profilepic} alt="Brian's profile pic" name="Brian"/>
 
             </AvatarGroup>
+
+
+
+            <VStack
+                alignItems={"start"}
+                gap={2}
+                mx={"auto"}
+                flex={1}
+            >
+
+                <Flex 
+                    gap={4} 
+                    direction={{base: "column", sm: "row"}}
+                    justifyContent={{base: "center", sm: "start"}}
+                    alignItems={"center"}
+                    w={"full"}
+                >
+
+                    <Text fontSize={{ base: "sm", md: "lg" }}>BrianSimiyu</Text>
+
+                    <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+
+                        <Button 
+                            bg={"white"}
+                            color={"black"}
+                            _hover={{ bg: "whiteAlpha.800" }}
+                            size={{ base: "xs", md: "sm" }}
+                        >Edit Profile</Button>
+
+                    </Flex>
+
+                </Flex>
+
+            </VStack>
 
         </Flex>
 
