384f7171f385a3245ca26e138756e3f422e67e4e
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-19 08:49:11 +0300

    Profile Posts Loading State

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,8 +1,20 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, VStack, Skeleton, Box } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
 
 const ProfilePosts = () =>{
 
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() =>{
     
+        setTimeout(() =>{
+
+            setIsLoading(false)
+
+        }, 2000)
+    
+    }, [])
 
     return(
 
@@ -15,7 +27,23 @@ const ProfilePosts = () =>{
             columnGap={1}
         >
 
+            {
 
+                isLoading && [0, 1, 2, 3, 4, 5].map(i =>(
+
+                    <VStack key={i} gap={4} alignItems={"start"} mb={10}>
+
+                        <Skeleton w={"full"}>
+
+                            <Box h={"500px"}>Contents wrapped</Box>
+
+                        </Skeleton>
+
+                    </VStack>
+
+                ))
+
+            }
 
         </Grid>
 
