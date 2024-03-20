018c45d44f6924f3fa4b23d143711aaf04472732
Brian Simiyu <briansimiyuj@gmail.com>
2024-03-20 12:45:18 +0300

    Comments structure 2

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,12 +1,27 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Box, Avatar, Text } from '@chakra-ui/react'
 
 const Comments = ({ createdAt, text, username, profilePic }) =>{
 
     return(
 
-        <Flex>
+        <Flex gap={4}>
 
-            Comments
+            <Avatar src={profilePic} name={username} alt={username} size={"sm"}/>
+
+            
+            <Flex direction={"column"}>
+
+                <Flex gap={2}>
+
+                    <Text fontWeight={"bold"} fontSize={12}>{username}</Text>
+
+                    <Text fontSize={14}>{text}</Text>
+
+                </Flex>
+
+                <Text fontSize={12} color={"gray"}>{createdAt}</Text>
+
+            </Flex>
 
         </Flex>
 
