a522bfd421fe3a0aae181e57c39fb89830484358
Brian Simiyu <briansimiyuj@gmail.com>
2024-04-15 08:28:35 +0300

    Fixing Search Component

--- src/components/Menu/Search.jsx
+++ src/components/Menu/Search.jsx
@@ -1,37 +1,47 @@
-import { Flex, Tooltip, Text } from "@chakra-ui/react"
+import { Flex, Tooltip, Text, useDisclosure } from "@chakra-ui/react"
 import { SearchLogo } from "../../assets/constants"
+import { useSearchUser } from "../../hooks/useSearchUser"
 
 const Search = () =>{
 
+    const { loading, searchUser, user } = useSearchUser(),
+          { isOpen, onOpen, onClose } = useDisclosure()
+
     return(
 
-        <Tooltip
-            hasArrow 
-            label="Search"
-            placement="right"
-            openDelay={500}
-            ml={1}
-            display={{base: "block", md: "none"}}
-        >
-
-            <Flex
-                alignItems="center"
-                justifyContent={{ base: "center", md: "start" }}
-                p={2}
-                gap={4}
-                _hover={{bg: "whiteAlpha.400"}}
-                borderRadius={6}
-                w={{ base: "100%", md: "full" }}
-                cursor="pointer"
+        <>
+        
+            <Tooltip
+                hasArrow 
+                label="Search"
+                placement="right"
+                openDelay={500}
+                ml={1}
+                display={{base: "block", md: "none"}}
             >
 
-                <SearchLogo/>
+                <Flex
+                    alignItems="center"
+                    justifyContent={{ base: "center", md: "start" }}
+                    p={2}
+                    gap={4}
+                    _hover={{bg: "whiteAlpha.400"}}
+                    borderRadius={6}
+                    w={{ base: "100%", md: "full" }}
+                    cursor="pointer"
+                    onClick={onOpen}
+                >
 
-                <Text display={{ base: "none", md: "block" }}>Search</Text>
+                    <SearchLogo/>
 
-            </Flex>
+                    <Text display={{ base: "none", md: "block" }}>Search</Text>
 
-        </Tooltip>
+                </Flex>
+
+            </Tooltip>
+        
+        </>
+
 
     )
 
