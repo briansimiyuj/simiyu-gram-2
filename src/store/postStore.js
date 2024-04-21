16c164b2e712d5db4b3297f7fddf8a3879642665
Brian Simiyu <briansimiyuj@gmail.com>
2024-04-21 15:26:54 +0300

    Set posts on post store

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -3,6 +3,7 @@ import { create } from "zustand"
 export const usePostStore = create((set) => ({
     
     posts: [],
-    createPost: (post) => set(state => ({ posts: [post, ...state.posts] }))
+    createPost: (post) => set(state => ({ posts: [post, ...state.posts] })),
+    setPosts: (posts) => set({ posts })
 
 }))
