4323f3466f60454d9357bbdc1c44b3ecaa5bf95d
Brian Simiyu <briansimiyuj@gmail.com>
2024-04-22 16:27:34 +0300

    Delete post store

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -4,6 +4,7 @@ export const usePostStore = create((set) => ({
     
     posts: [],
     createPost: (post) => set(state => ({ posts: [post, ...state.posts] })),
-    setPosts: (posts) => set({ posts })
+    setPosts: (posts) => set({ posts }),
+    deletePost: (id) => set(state => ({ posts: state.posts.filter(post => post.postId !== id) }))
 
 }))
