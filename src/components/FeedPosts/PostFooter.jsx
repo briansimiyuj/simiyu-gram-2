3409089817c5c92d617b7b5d59245a42836859d9
Brian Simiyu <briansimiyuj@gmail.com>
2024-04-26 13:27:41 +0300

    Post footer on Profile page vs on Home page

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -41,129 +41,258 @@ const PostFooter = ({ username, isProfilePage, marginTop, post }) =>{
 
     return(
 
-        <Box position="absolute" width={"100%"} bottom={0} mb={10} ml={-9}>
-        
-            <Flex
-                alignItems={"center"}
-                gap={4}
-                w={"full"}
-                pt={0}
-                mb={2}
-                mt={3}
-            >
-
-                <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
-
-                    {
-
-                        !liked? (<NotificationsLogo/>) : (<UnlikeLogo/>)
-
-                    }
-
-                </Box>
-
-
-                <Box cursor={"pointer"} fontSize={18}>
-
-                    <CommentLogo/>
-
-                </Box>
-
-            </Flex>
-
-
-            <Text fontWeight={600} fontSize={"sm"}>{likes} likes</Text>
-
-
+       <>
+       
             {
 
-                !isProfilePage && (
-
-                    <>
+                isProfilePage ?(
                     
-                        <Text fontSize='sm' fontWeight={700}>   
-
-                            {username}{" "}
-
-                            <Text as="span" fontWeight={400}>Feeling Good</Text>
-
-                            <Text fontSize='sm' color={"gray"}>View all 1,000 comments</Text>
-
-                        </Text>
+                    <Box position="absolute" width={"100%"} bottom={0} mb={10} ml={-9}>
                     
-                    </>
+                        <Flex
+                            alignItems={"center"}
+                            gap={4}
+                            w={"full"}
+                            pt={0}
+                            mb={2}
+                            mt={3}
+                        >
 
-                )
+                            <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
 
-            }
-
-
-            
-            {
-            
-                authUser && (
-                    
-                    <Flex
-                        alignItems={"center"}
-                        gap={2} 
-                        justifyContent={"space-between"}
-                        w={"full"}
-                    >
-
-                        <InputGroup>
-                        
-                            <Input 
-                                variant={"flushed"} 
-                                placeholder={"Add a comment..."} 
-                                fontSize={14}
-                                value={userComment}
-                                onChange={e => setUserComment(e.target.value)}
-                            />
-
-                            <InputRightElement>
-                            
                                 {
 
-                                    !userComment ? (
-
-                                        <Button
-                                            fontSize={14}
-                                            color={"gray.500"}
-                                            fontWeight={600}
-                                            cursor={"not-allowed"}
-                                            _hover={{color: "white"}}
-                                            bg={"transparent"}
-                                            disabled
-                                        >Post</Button>
-
-                                    ):(
-
-                                        <Button
-                                            fontSize={14}
-                                            color={"blue.500"}
-                                            fontWeight={600}
-                                            cursor={"pointer"}
-                                            _hover={{color: "white"}}
-                                            bg={"transparent"}
-                                            onClick={handleSubmitComment}
-                                            isLoading={commenting}
-                                        >Post</Button>
-
-                                    )
+                                    !liked? (<NotificationsLogo/>) : (<UnlikeLogo/>)
 
                                 }
+
+                            </Box>
+
+
+                            <Box cursor={"pointer"} fontSize={18}>
+
+                                <CommentLogo/>
+
+                            </Box>
+
+                        </Flex>
+
+
+                        <Text fontWeight={600} fontSize={"sm"}>{likes} likes</Text>
+
+
+                        {
+
+                            !isProfilePage && (
+
+                                <>
+                                
+                                    <Text fontSize='sm' fontWeight={700}>   
+
+                                        {username}{" "}
+
+                                        <Text as="span" fontWeight={400}>Feeling Good</Text>
+
+                                        <Text fontSize='sm' color={"gray"}>View all 1,000 comments</Text>
+
+                                    </Text>
+                                
+                                </>
+
+                            )
+
+                        }
+
+
+                        
+                        {
+                        
+                            authUser && (
+                                
+                                <Flex
+                                    alignItems={"center"}
+                                    gap={2} 
+                                    justifyContent={"space-between"}
+                                    w={"full"}
+                                >
+
+                                    <InputGroup>
+                                    
+                                        <Input 
+                                            variant={"flushed"} 
+                                            placeholder={"Add a comment..."} 
+                                            fontSize={14}
+                                            value={userComment}
+                                            onChange={e => setUserComment(e.target.value)}
+                                        />
+
+                                        <InputRightElement>
+                                        
+                                            {
+
+                                                !userComment ? (
+
+                                                    <Button
+                                                        fontSize={14}
+                                                        color={"gray.500"}
+                                                        fontWeight={600}
+                                                        cursor={"not-allowed"}
+                                                        _hover={{color: "white"}}
+                                                        bg={"transparent"}
+                                                        disabled
+                                                    >Post</Button>
+
+                                                ):(
+
+                                                    <Button
+                                                        fontSize={14}
+                                                        color={"blue.500"}
+                                                        fontWeight={600}
+                                                        cursor={"pointer"}
+                                                        _hover={{color: "white"}}
+                                                        bg={"transparent"}
+                                                        onClick={handleSubmitComment}
+                                                        isLoading={commenting}
+                                                    >Post</Button>
+
+                                                )
+
+                                            }
+                                        
+                                        </InputRightElement>
+                                    
+                                    </InputGroup>
+                                    
+                                </Flex>
+
+                            )
+                        
+                        }
+                    
+                    </Box>
+                    
+                ):(
+
+                    <Box marginTop={marginTop} mb={10}>
+        
+                        <Flex
+                            alignItems={"center"}
+                            gap={4}
+                            w={"full"}
+                            pt={0}
+                            mb={2}
+                            mt={3}
+                        >
+
+                            <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+
+                                {
+
+                                    !liked? (<NotificationsLogo/>) : (<UnlikeLogo/>)
+
+                                }
+
+                            </Box>
+
+
+                            <Box cursor={"pointer"} fontSize={18}>
+
+                                <CommentLogo/>
+
+                            </Box>
+
+                        </Flex>
+
+
+                        <Text fontWeight={600} fontSize={"sm"}>{likes} likes</Text>
+
+
+                        {
+
+                            !isProfilePage && (
+
+                                <>
+                                
+                                    <Text fontSize='sm' fontWeight={700}>   
+
+                                        {username}{" "}
+
+                                        <Text as="span" fontWeight={400}>Feeling Good</Text>
+
+                                        <Text fontSize='sm' color={"gray"}>View all 1,000 comments</Text>
+
+                                    </Text>
+                                
+                                </>
+
+                            )
+
+                        }
+
+
+                        <Flex
+                            alignItems={"center"}
+                            gap={2} 
+                            justifyContent={"space-between"}
+                            w={"full"}
+                        >
+
+                            <InputGroup>
                             
-                            </InputRightElement>
-                        
-                        </InputGroup>
-                        
-                    </Flex>
+                                <Input 
+                                    variant={"flushed"} 
+                                    placeholder={"Add a comment..."} 
+                                    fontSize={14}
+                                    value={userComment}
+                                    onChange={e => setUserComment(e.target.value)}
+                                />
+
+                                <InputRightElement>
+                                
+                                    {
+
+                                        !userComment ? (
+
+                                            <Button
+                                                fontSize={14}
+                                                color={"gray.500"}
+                                                fontWeight={600}
+                                                cursor={"not-allowed"}
+                                                _hover={{color: "white"}}
+                                                bg={"transparent"}
+                                                disabled
+                                            >Post</Button>
+
+                                        ):(
+
+                                            <Button
+                                                fontSize={14}
+                                                color={"blue.500"}
+                                                fontWeight={600}
+                                                cursor={"pointer"}
+                                                _hover={{color: "white"}}
+                                                bg={"transparent"}
+                                                onClick={handleSubmitComment}
+                                                isLoading={commenting}
+                                            >Post</Button>
+
+                                        )
+
+                                    }
+                                
+                                </InputRightElement>
+                            
+                            </InputGroup>
+                            
+                        </Flex>
+                    
+                    </Box>
 
                 )
-            
+
             }
-        
-        </Box>
+       
+       </> 
 
     )
 
