537fc4f34f7753e909a2785379b4fb5ad420cbc0
Brian Simiyu <briansimiyuj@gmail.com>
2024-05-01 17:11:20 +0300

    Suggested Users Skeleton

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -2,12 +2,39 @@ import { Box, Flex, Link, Text, VStack } from '@chakra-ui/react'
 import { useGetSuggestedUsers } from '../../hooks/useGetSuggestedUsers'
 import SuggestedHeader from './SuggestedHeader'
 import SuggestedUser from './SuggestedUser'
+import SuggestedUsersSkeleton from './SuggestedUsersSkeleton'
 
 const SuggestedUsers = () =>{
 
     const { loading, suggestedUsers } = useGetSuggestedUsers()
 
-    if(loading) return null //TODO
+    if(loading){
+
+        return(
+
+            <VStack py={8} px={6} gap={4}>
+
+                <SuggestedHeader/>
+
+                <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+
+                    <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>Suggested For You</Text>
+
+                    <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>See All</Text>
+
+                </Flex>
+
+                {
+                
+                    [...Array(3)].map((_, i) => <SuggestedUsersSkeleton key={i} height="60px"/>)
+                    
+                }
+
+            </VStack>
+
+        )
+
+    }
 
     return(
 
---
+++ src/components/SuggestedUsers/SuggestedUsersSkeleton.jsx
@@ -0,0 +1,30 @@
+import { Flex, Skeleton, SkeletonCircle} from "@chakra-ui/react"
+
+const SuggestedUsersSkeleton = () =>{
+
+    return(
+
+        <>
+
+            <Flex gap={4} w={"full"} alignItems={"center"}>
+
+                <SkeletonCircle h={10} w='10'/>
+
+
+                <Flex gap={1} flexDir={"column"}>
+
+                    <Skeleton height={2} width={100} />
+
+                    <Skeleton height={2} width={50} />
+
+                </Flex>
+
+            </Flex>
+        
+        </>
+
+    )
+
+}
+
+export default SuggestedUsersSkeleton
