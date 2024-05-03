79973f5dc658ae105cdaae0b0ce16c7ef096cbb4
Brian Simiyu <briansimiyuj@gmail.com>
2024-05-03 10:27:57 +0300

    Remove suggested users 1

--- src/components/SuggestedUsers/SuggestedUserTwo.jsx
+++ src/components/SuggestedUsers/SuggestedUserTwo.jsx
@@ -1,12 +1,14 @@
-import { VStack, Flex, Avatar, Text, Button, Link } from '@chakra-ui/react'
+import { VStack, Flex, Avatar, Text, Button, Link, CloseButton } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 import { useFollowUser } from '../../hooks/useFollowUser'
 import { useAuthStore } from '../../store/authStore'
+import { useState } from 'react'
 
 const SuggestedUserTwo = ({ user, setUser, onClose }) =>{
 
     const { isUpdating, isFollowingUser, handleFollowUser } = useFollowUser(user?.userId),
-          authUser = useAuthStore(state => state.user)
+          authUser = useAuthStore(state => state.user),
+          [suggestedUser, setSuggestedUser] = useState(true)
 
     
     const onFollowUser = async() =>{
@@ -25,53 +27,71 @@ const SuggestedUserTwo = ({ user, setUser, onClose }) =>{
 
     return(
 
-        <Flex 
-            mb={5}
-            justifyContent={"space-between"}    
-            w={"60%"}
-            alignItems={"center"} 
-            _hover={{ bg: "whiteAlpha.300" }}
-            paddingInline={2}
-        >
+        <>
+        
+            {
+            
+                suggestedUser &&(
 
-            <Link 
-                to={`/${user?.username}`} 
-                as={RouterLink}
-                _hover={{textDecoration: "none"}}
-                onClick={onClose}
-            >
+                    <Flex 
+                        mb={5}
+                        justifyContent={"space-between"}    
+                        w={"60%"}
+                        alignItems={"center"} 
+                        _hover={{ bg: "whiteAlpha.300" }}
+                        paddingInline={2}
+                    >
 
-                <Flex alignItems={"center"} gap={2}>
+                        <Link 
+                            to={`/${user?.username}`} 
+                            as={RouterLink}
+                            _hover={{textDecoration: "none"}}
+                            onClick={onClose}
+                        >
 
-                    <Avatar size={"md"} src={user?.profilePicURL} name={user?.fullName}/>
+                            <Flex alignItems={"center"} gap={2}>
 
-
-                    <VStack spacing={2} alignItems={"start"}>
-
-                        <Text fontSize={14} fontWeight={"bold"}>{user?.username}</Text>
-
-                        <Text fontSize={12} color={"gray.500"}>{user?.followers.length} followers</Text>
-
-                    </VStack>
-
-                </Flex>
-
-            </Link>
+                                <Avatar size={"md"} src={user?.profilePicURL} name={user?.fullName}/>
 
 
-            <Button
-                fontSize={14}
-                bg={"blue.400"}
-                p={3}
-                fontWeight={"medium"}
-                color={"white"}
-                cursor={"pointer"}
-                _hover={{ color: "white" }}
-                onClick={onFollowUser}
-                isLoading={isUpdating}
-            >Follow</Button>
+                                <VStack spacing={2} alignItems={"start"}>
 
-        </Flex>
+                                    <Text fontSize={14} fontWeight={"bold"}>{user?.username}</Text>
+
+                                    <Text fontSize={12} color={"gray.500"}>{user?.followers.length} followers</Text>
+
+                                </VStack>
+
+                            </Flex>
+
+                        </Link>
+
+                        <Flex gap={2} alignItems={"center"}>
+
+                            <Button
+                                fontSize={14}
+                                bg={"blue.400"}
+                                p={3}
+                                fontWeight={"medium"}
+                                color={"white"}
+                                cursor={"pointer"}
+                                _hover={{ color: "white" }}
+                                onClick={onFollowUser}
+                                isLoading={isUpdating}
+                            >Follow</Button>
+
+                            <CloseButton onClick={() => setSuggestedUser(false)}/>
+
+                        </Flex>
+
+                    </Flex>
+
+                )
+
+            }
+        
+        </>
+
 
     )
 
