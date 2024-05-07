eb2fb4ceca6e1c56e09b2cf4d499b7aa6844e00d
Brian Simiyu <briansimiyuj@gmail.com>
2024-05-07 14:58:31 +0300

    Edit and delete button on PostHeaderProfile

--- src/components/FeedPosts/PostHeaderProfile.jsx
+++ src/components/FeedPosts/PostHeaderProfile.jsx
@@ -1,43 +1,76 @@
-import { Flex, Box, Avatar, Text, Link } from '@chakra-ui/react'
+import { Flex, Box, Avatar, Text, Link, Button } from '@chakra-ui/react'
+import { MdEdit, MdDelete } from 'react-icons/md'
 import { Link as RouterLink } from 'react-router-dom'
 import { timeAgo } from '../../utils/timeAgo'
+import { useAuthStore } from '../../store/authStore'
 
 
 const PostHeaderProfile = ({ post, userProfile }) =>{
+
+    const authUser = useAuthStore(state => state.user)
 
     return(
 
         <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
 
-            <Flex alignItems={"center"} gap={2}>
+            <Flex alignItems={"center"}  w={"full"} justifyContent={"space-between"}>
 
-                <Link
-                    fontSize={14}
-                    fontWeight={700}
-                    as={RouterLink}
-                    display="flex"
-                    gap={3}
-                    to={`/${userProfile?.username}`}
-                >
-                    
-                    <Avatar 
-                        src={userProfile?.profilePicURL} 
-                        alt={userProfile?.profilePicURL} 
-                        size={"sm"} 
-                        name={userProfile?.fullName}
-                    />
-
-                    <Text mt={1}>{userProfile?.username}</Text>
-                
-                </Link>
 
                 <Flex fontSize={12} fontWeight={"bold"} gap={2}>
+                
+                    <Link
+                        fontSize={14}
+                        fontWeight={700}
+                        as={RouterLink}
+                        display="flex"
+                        gap={3}
+                        to={`/${userProfile?.username}`}
+                    >
+                        
+                        <Avatar 
+                            src={userProfile?.profilePicURL} 
+                            alt={userProfile?.profilePicURL} 
+                            size={"sm"} 
+                            name={userProfile?.fullName}
+                        />
 
-                    <Box color={"gray.500"}>{ timeAgo(post.createdAt) }</Box>
+                        <Text mt={1}>{userProfile?.username}</Text>
+                    
+                    </Link>
+
+                    <Box color={"gray.500"} mt={1.5}>{ timeAgo(post.createdAt) }</Box>
 
                 </Flex>
 
-            </Flex>
+
+                {
+
+                    authUser?.userId === userProfile?.userId &&(
+
+
+                        <Flex gap={2}>
+
+                            <Button size="sm">
+
+                                <MdEdit/>
+
+                            </Button>
+                            
+                            
+                            <Button size="sm">
+
+                                <MdDelete/>
+
+                            </Button>
+
+                        </Flex>
+
+                    )
+
+                }
+
+            </Flex>           
+
 
         </Flex>
 
